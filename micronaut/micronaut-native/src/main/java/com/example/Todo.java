6cccda68db277d2331229d390025baa051efb4ac
Philipp Heringer <phr@novatec-gmbh.de>
2025-04-24 10:33:12 +0200

    micronaut native

---
+++ micronaut/micronaut-native/src/main/java/com/example/Todo.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import io.micronaut.configuration.hibernate.jpa.proxy.GenerateProxy;
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Serdeable
+@Entity
+@GenerateProxy
+@Table(name = "/todo")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "description")
+    private String description;
+
+    public Todo() {}
+    public Todo(@NotNull String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
